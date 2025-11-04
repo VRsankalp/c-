7ca03dd37a60dfe5f2dff0dc30ebae45a9a1e3a1
Sankalp Srivastava <srivastavasankalp509@gmail.com>
2025-11-04 10:58:37 +0530

    doblelinkedlist

---
+++ DSA/LinkedList/DoubleLinkedList.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std ;
+class Node{
+    public:
+    int data ;
+    Node*next;
+    Node*pre;
+
+    Node(int val ){
+        data = val ;
+        next = NULL;
+        pre = NULL;
+    }
+    
+    
+};
+class DoubleList{
+    public:
+    Node*head ,*tail;
+    DoubleList(){
+        head = tail = NULL;
+    }
+    void addFront(int val){
+        Node*newnode = new Node(val);
+        if(head == NULL){
+            head = tail =newnode;
+        }
+        else{
+            newnode->next=head;
+            head->pre=newnode;
+            head = newnode;
+        }
+
+    }
+
+
+    
+
+
+};
