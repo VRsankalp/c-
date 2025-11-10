67186d7ab0b113c576acc0b001ae66c54ed98429
Sankalp Srivastava <srivastavasankalp509@gmail.com>
2025-11-10 10:27:36 +0530

    stackwitharaaya

---
+++ Stack/stackThroughArray.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std ;
+class Stack{
+    int *arr;
+    int capacity;
+    int top ;
+    public:
+        Stack(int size){
+            arr = new int[size];
+            capacity=size;
+            top = -1 ;
+
+        }
+        void push(int val){
+          if (top == capacity-1){
+            cout<<"stack is full";
+          }
+            arr[++top]=val;
+
+        }
+        void pop(){
+            if(top==-1){
+                cout<<"stack is under flow";
+            }
+            
+            --top;
+           
+        } 
+        void peek(){
+            cout<<top<<"this is the top element ";
+        }
+        bool isEmpty(){
+            return top==-1;
+        }
+};
+int main(){
+    Stack s(4);
+    s.push(2);
+    s.push(3);
+    s.push(24);
+    s.push(243);
+
+}
